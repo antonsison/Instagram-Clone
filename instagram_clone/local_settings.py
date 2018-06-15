32ed428e23324e80861e73df276c8c2432d9f7d5
Anton Sison <antoninakisison@gmail.com>
2018-06-15 15:36:22 +0800

    Added local_settings.py

---
+++ instagram_clone/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'instagram_clone',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
